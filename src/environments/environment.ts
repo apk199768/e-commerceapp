e59be6857690be0ee260015b3b8f8c6d93fdfe17
apk199768 <143738549+apk199768@users.noreply.github.com>
2023-11-06 15:35:28 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {    production:false,
-    apkshopApiUrl:"https://localhost:8443/api",
+    apkshopApiUrl:"https://backendapplicationa.azurewebsites.net/api",
 stripePublishableKey:"pk_test_51O7fEiB3FFEPWuS09WICbPtXDU8NJJZvrqTc126pPckxMAmbbwiI6YhamruYkRhF6WfSSG2I9rpsE1ClZGpgCG9V00AbNkoaDO"};
